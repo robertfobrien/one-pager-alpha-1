68a2620be841ddb1c4361d20334d71f47cb728c5
jfobrien29 <thejackobrien29@gmail.com>
2020-06-19 17:16:18 -0400

    Sample feature

--- components/Home.tsx
+++ components/Home.tsx
@@ -32,7 +32,7 @@ export const Home = () => {
       <Box d='flex' justifyContent='center'>
         <Box w='xl'>
           <Heading as='h1' size='xl'>
-            Welcome to One Pager Alpha!
+            Welcome to One Pager Alpha V2!
           </Heading>
 
           <Heading as='h2' size='md'>
